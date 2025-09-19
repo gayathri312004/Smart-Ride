6273392f5cbe0b3f849882f9e671c0b1c654c7d8
M.Gayathri <mgayathri312004@gmail.com>
2025-09-19 21:49:03 +0530

    Add files via upload

---
+++ server/config/passport.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const passport = require('passport');
+const User = require('../models/User');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        // Check if user already exists
+        let user = await User.findOne({ email: profile.emails[0].value });
+        
+        if (user) {
+          // User exists, return user
+          return done(null, user);
+        } else {
+          // Create new user
+          user = await User.create({
+            name: profile.displayName,
+            email: profile.emails[0].value,
+            googleId: profile.id,
+            role: 'passenger', // default role
+            // No password for Google users
+          });
+          return done(null, user);
+        }
+      } catch (error) {
+        return done(error, null);
+      }
+    }
+  )
+);
+
+// Save user to session
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
